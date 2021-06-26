632f2439bcba1e0db931c9bfe5085357d5ec3a51
André Gomes <andre.montero702@gmail.com>
2021-06-26 11:15:17 -0300

    Adicionado teste mockado para CI

---
+++ src/app/admin/users/services/users.service.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UsersService } from './users.service';
+
+describe('Mocked test', () => {
+
+  it('should be created', () => {
+    expect(true).toBeTruthy();
+  });
+});
