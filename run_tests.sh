be17781658f26fe980ba79b3f98a008fc00d0837
André M Gomes <andre.montero702@gmail.com>
2021-12-01 06:22:07 -0300

    Adicionando script para criaçao de usuario. Consertando testes

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl 'http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXIaX1Y2g_jnIrvrewlVvmhR4iwpejxEc' \
+-H 'Content-Type: application/json' \
+--data-binary '{"email":"patricia@email.com","password":"123456"}'
+ng e2e
